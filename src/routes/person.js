4c56f38b56fb3763fa8ccc8cdc6dc1476436eb80
kiranchhablani <83265750+kiranchhablani@users.noreply.github.com>
2021-10-14 14:29:57 +0530

    routes by query string and params

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+//query string (after ? )
+//localhost:4000/person?name=kiran
 router.get('/person',(request,response) => {
+ if(request.query.name){
+  response.send(`you have requested a person ${request.query.name}`);
+ }
  response.send('you have requested a person');
 });
 
+//params property on the request object
+//localhost:4000/person/kiran
+router.get('/person/:name',(request,response) => {
+ response.send(`you have requested a person ${request.params.name}`);
+});
 module.exports = router;
