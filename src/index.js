9cf1cba1cfb7c1ee9b7f42c51d2f7366295e36a3
kiranchhablani <83265750+kiranchhablani@users.noreply.github.com>
2021-10-14 14:16:37 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+let personRoute = require('./routes/person');
 const app = express();
 
 app.use(express.static('public'));
+app.use(personRoute);
 const port = process.env.PORT || 4000 ;
 
 app.listen(port,()=>{
