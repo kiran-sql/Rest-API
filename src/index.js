8fccff36bb2fd6c485d760e1f21f84fafc2799b4
kiranchhablani <83265750+kiranchhablani@users.noreply.github.com>
2021-10-14 14:34:51 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 let personRoute = require('./routes/person');
 const app = express();
-
-app.use(express.static('public'));
+app.use((req,res,next) => {
+ console.log(`${new Date().toString()} => ${req.originalUrl}`);
+ next();
+});
 app.use(personRoute);
+app.use(express.static('public'));
 const port = process.env.PORT || 4000 ;
 
 app.listen(port,()=>{
